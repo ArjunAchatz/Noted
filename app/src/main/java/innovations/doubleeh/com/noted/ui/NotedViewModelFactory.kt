04a15d77019b6ecf25f678cce76dffce60185532
Arjun Achatz <arjun@matterandform.net>
2017-12-12 23:34:03 -0500

    100% Kotlin again .. phew

---
+++ app/src/main/java/innovations/doubleeh/com/noted/ui/NotedViewModelFactory.kt
@@ -0,0 +1,40 @@
+package innovations.doubleeh.com.noted.ui
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+/**
+ * Created by arjunachatz on 2017-12-12.
+ */
+
+
+@Singleton
+class NotedViewModelFactory @Inject
+constructor(val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) : ViewModelProvider.Factory {
+
+    @SuppressWarnings("unchecked")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("unknown model class " + modelClass)
+        }
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+    }
+}
