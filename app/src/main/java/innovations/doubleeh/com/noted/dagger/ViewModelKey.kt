f98b499f304086af74eeb2ed28430e740b7a2d9b
Arjun Achatz <arjun@matterandform.net>
2017-12-12 23:17:03 -0500

    Kotlinizing code

---
+++ app/src/main/java/innovations/doubleeh/com/noted/dagger/ViewModelKey.kt
@@ -0,0 +1,20 @@
+package innovations.doubleeh.com.noted.dagger
+
+import android.arch.lifecycle.ViewModel
+
+import java.lang.annotation.Documented
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+/**
+ * Created by arjunachatz on 2017-12-12.
+ */
+
+@Documented
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(RetentionPolicy.RUNTIME)
+@MapKey
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
---
+++ app/src/main/java/innovations/doubleeh/com/noted/dagger/ViewModelModule.kt
@@ -0,0 +1,26 @@
+package innovations.doubleeh.com.noted.dagger
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import innovations.doubleeh.com.noted.ui.NotedViewModelFactory
+import innovations.doubleeh.com.noted.ui.notedAdd.NotedAddViewModel
+
+/**
+ * Created by arjunachatz on 2017-12-12.
+ */
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(NotedAddViewModel::class)
+    abstract fun bindRepoViewModel(repoViewModel: NotedAddViewModel): ViewModel
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: NotedViewModelFactory): ViewModelProvider.Factory
+}
