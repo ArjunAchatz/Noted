6b10d64d0d1e6b64e8f546a91691d452c6ec2bea
Arjun Achatz <arjun@matterandform.net>
2017-12-21 13:54:14 -0500

    Moving logic of add/delete into viewmodel

--- app/src/main/java/innovations/doubleeh/com/noted/ui/notedList/NotedListViewModel.kt
+++ app/src/main/java/innovations/doubleeh/com/noted/ui/notedList/NotedListViewModel.kt
@@ -4,6 +4,10 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import innovations.doubleeh.com.noted.repository.Note
 import innovations.doubleeh.com.noted.repository.NotedDatabase
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
@@ -15,10 +19,39 @@ class NotedListViewModel @Inject constructor(val notedDatabase: NotedDatabase) :
     private var listOfNotes: LiveData<List<Note>>? = null
 
     fun getLiveDataListOfNotes(): LiveData<List<Note>> {
-        if(listOfNotes == null) {
+        if (listOfNotes == null) {
             listOfNotes = notedDatabase.notesDao().allNotes()
         }
         return listOfNotes!!
+    }
+
+    fun deleteNote(note: Note) {
+        Observable
+                .just(0)
+                .observeOn(Schedulers.io())
+                .subscribe(object: Observer<Int>{
+                    override fun onError(e: Throwable) {}
+                    override fun onSubscribe(d: Disposable) {}
+                    override fun onComplete() {}
+                    override fun onNext(t: Int) {
+                        notedDatabase.notesDao().delete(note)
+                    }
+
+                })
+    }
+
+    fun insertNote(note: Note) {
+        Observable
+                .just(0)
+                .observeOn(Schedulers.io())
+                .subscribe(object: Observer<Int>{
+                    override fun onError(e: Throwable) {}
+                    override fun onSubscribe(d: Disposable) {}
+                    override fun onComplete() {}
+                    override fun onNext(t: Int) {
+                        notedDatabase.notesDao().insert(note)
+                    }
+                })
     }
 
 
