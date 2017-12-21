4d94e518d7940b7b6a222128a211645a45641555
Arjun Achatz <arjun@matterandform.net>
2017-12-21 13:48:14 -0500

    Correcting DiffUtils constructor or old vs new lists

--- app/src/main/java/innovations/doubleeh/com/noted/ui/notedList/NoteListDiffUtils.kt
+++ app/src/main/java/innovations/doubleeh/com/noted/ui/notedList/NoteListDiffUtils.kt
@@ -15,10 +15,10 @@ class NoteListDiffUtils(private val oldNoteList: List<Note>, private val newNote
     override fun getNewListSize(): Int = newNoteList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldNoteList[oldItemPosition].id != newNoteList[newItemPosition].id
+        return oldNoteList[oldItemPosition].id == newNoteList[newItemPosition].id
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldNoteList[oldItemPosition] != newNoteList[newItemPosition]
+        return oldNoteList[oldItemPosition] == newNoteList[newItemPosition]
     }
 }
